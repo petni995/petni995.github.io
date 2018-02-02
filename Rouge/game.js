92db2c585ec48a4b2f49dee56d4c077f48c91efd
Peter Nilsson <petni995@gmail.com>
2018-02-02 16:07:55 +0100

    fonsize test

--- Rouge/game.js
+++ Rouge/game.js
@@ -4,6 +4,10 @@ var Game =  {
 	init: function() {
 	    // Any necessary initialization will go here.
 	    this._display = new ROT.Display({width: 80, height: 24});
+			this._display.setOptions({
+			    fontSize: 18
+
+			});
 	    // Create a helper function for binding to an event
 	    // and making it send it to the screen
 	    var game = this; // So that we don't lose this
