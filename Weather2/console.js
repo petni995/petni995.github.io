732e9b9890cae338f353b2e26ddec9a1485b7ad2
Peter Nilsson <petni995@gmail.com>
2018-02-27 12:56:20 +0100

    fix temp

--- Weather2/console.js
+++ Weather2/console.js
@@ -83,7 +83,9 @@ function update() {
     tempD = Math.round(weatherData[i]['t']) + ""
     display.drawText(11,  4 + i, tempD);
 
-    if (tempD > -15 && tempD <= -10) {
+    if (tempD > -20 && tempD <= -15) {
+          display.drawText(14,  4 + i, "[%b{#0040ff}-%b{}---|------]");
+    } else if (tempD > -15 && tempD <= -10) {
           display.drawText(14,  4 + i, "[-%b{#0080ff}-%b{}--|------]");
     } else if (tempD > -10 && tempD <= -5) {
           display.drawText(14,  4 + i, "[--%b{#00bfff}-%b{}-|------]");
