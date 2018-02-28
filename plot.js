97c5ad8ce147607eb57eefbaeda3584873064260
Peter Nilsson <petni995@gmail.com>
2018-02-28 09:38:03 +0100

    snjodjup

--- plot.js
+++ plot.js
@@ -630,3 +630,103 @@ svg.append("text")
         .text(title);
 
 }
+
+function plotDualMonth3(dataset, dataset2, title) {
+
+// Define the resolution
+var width = 800;
+var height = 400;
+
+// Create the SVG 'canvas'
+var svg = d3.select("#chart")
+    .append("svg")
+    .attr("width", width )
+    .attr("height", height)
+
+
+// Define the padding around the graph
+var padding = 60;
+
+// Set the scales
+var minDate = d3.min(dataset, function(d) { return d.x; });
+// minDate.setDate(minDate.getDate() - 1);
+
+var maxDate = d3.max(dataset, function(d) { return d.x; });
+
+var xScale = d3.time.scale()
+    .domain([minDate, maxDate])
+    .range([padding, width - padding]);
+
+var yScale = d3.scale.linear()
+    .domain([d3.min(dataset, function(d) { return d.y; }), d3.max(dataset, function(d) { return d.y; })])
+    .range([height - padding, padding]);
+
+// x-axis
+var format = d3.time.format("%d %b");
+var xAxis = d3.svg.axis()
+    .scale(xScale)
+    .orient("bottom")
+    .tickFormat(format)
+    .ticks(d3.time.weeks, 1);
+
+svg.append("g")
+    .attr("class", "axis x-axis")
+    .attr("transform", "translate(0," + (height - padding) + ")")
+    .call(xAxis)
+    .selectAll("text")
+    .attr("y", 0)
+    .attr("x", 9)
+    .attr("dy", ".35em")
+    .attr("transform", "rotate(90)")
+    .style("text-anchor", "start");
+
+// y-axis
+var yAxis = d3.svg.axis()
+    .scale(yScale)
+    .orient("left")
+    .tickFormat(function (d) { return d; })
+    .tickSize(5, 5, 0)
+    .ticks(5); // set rough # of ticks
+
+svg.append("g")
+    .attr("class", "axis y-axis")
+    .attr("transform", "translate(" + padding + ",0)")
+    .call(yAxis);
+
+
+// plot circles
+svg.selectAll(".data-point")
+    .data(dataset)
+    .enter()
+    .append("circle")
+    .attr("class", "data-point")
+    .attr("cx", function(d) {
+        return xScale(d.x);
+    })
+    .attr("cy", function(d) {
+        return yScale(d.y);
+    })
+    .attr("r", 3);
+
+svg.selectAll(".data-point2")
+    .data(dataset2)
+    .enter()
+    .append("circle")
+    .attr("class", "data-point2")
+    .attr("cx", function(d) {
+        return xScale(d.x);
+    })
+    .attr("cy", function(d) {
+        return yScale(d.y);
+    })
+    .attr("r", 3);
+
+svg.append("text")
+        .attr("x", (width / 2))
+        .attr("y", 50 )
+        .attr("text-anchor", "middle")
+        .style("font-size", "16px")
+        .style("text-decoration", "underline")
+        .text(title);
+
+}
--- readData.js
+++ readData.js
@@ -156,3 +156,23 @@ $.getJSON( "https://opendata-download-metobs.smhi.se/api/version/latest/paramete
       });
   });
 });
+
+// Snödjup
+
+$.getJSON( "https://opendata-download-metobs.smhi.se/api/version/latest/parameter/8/station/98210/period/latest-months/data.json", function( data ) {
+  var plot1 = []
+  var plot2 = []
+  var observTemp = data
+  for (var i = 0; i < observTemp.value.length; i++) {
+    var d = new Date(observTemp.value[i].date)
+    plot1.push({'x': d,'y':Number(observTemp.value[i].value)})
+  }
+  $.getJSON( "https://opendata-download-metobs.smhi.se/api/version/latest/parameter/8/station/98210/period/latest-day/data.json", function( data ) {
+    var observTemp2 = data
+    for (var i = 0; i < observTemp2.value.length; i++) {
+      var d = new Date(observTemp2.value[i].date)
+      plot2.push({'x': d,'y':Number(observTemp2.value[i].value)})
+    }
+    plotDualMonth3(plot1, plot2, "snödjup latest months - Sthlm");
+  });
+});
