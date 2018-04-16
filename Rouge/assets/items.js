121533aa08b970684bba967d4f4e478e9709b512
Peter Nilsson <petni995@gmail.com>
2018-04-16 09:51:44 +0200

    bug fix

--- Rouge/assets/items.js
+++ Rouge/assets/items.js
@@ -4,6 +4,7 @@ Game.ItemRepository.define('apple', {
     name: 'apple',
     character: '%',
     foreground: 'red',
+    background: 'white',
     foodValue: 50,
     mixins: [Game.ItemMixins.Edible]
 });
@@ -12,6 +13,7 @@ Game.ItemRepository.define('melon', {
     name: 'melon',
     character: '%',
     foreground: 'brightGreen',
+    background: 'white',
     foodValue: 35,
     consumptions: 4,
     mixins: [Game.ItemMixins.Edible]
