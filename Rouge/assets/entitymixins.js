4f8223642094ae3a16624346ab908ae01bbd21e2
Peter Nilsson <petni995@gmail.com>
2018-05-02 14:04:05 +0200

    part 12 - equip functions

--- Rouge/assets/entitymixins.js
+++ Rouge/assets/entitymixins.js
@@ -114,9 +114,20 @@ Game.EntityMixins.Destructible = {
       return this._maxHp;
   },
   getDefenseValue: function() {
-      return this._defenseValue;
-  },
-  takeDamage: function(attacker, damage) {
+        var modifier = 0;
+        // If we can equip items, then have to take into
+        // consideration weapon and armor
+        if (this.hasMixin(Game.EntityMixins.Equipper)) {
+            if (this.getWeapon()) {
+                modifier += this.getWeapon().getDefenseValue();
+            }
+            if (this.getArmor()) {
+                modifier += this.getArmor().getDefenseValue();
+            }
+        }
+        return this._defenseValue + modifier;
+    },
+    takeDamage: function(attacker, damage) {
       this._hp -= damage;
       // If have 0 or less HP, then remove ourseles from the map
       if (this._hp <= 0) {
@@ -138,7 +149,18 @@ Game.EntityMixins.Attacker = {
       this._attackValue = template['attackValue'] || 1;
     },
     getAttackValue: function() {
-      return this._attackValue;
+        var modifier = 0;
+        // If we can equip items, then have to take into
+        // consideration weapon and armor
+        if (this.hasMixin(Game.EntityMixins.Equipper)) {
+            if (this.getWeapon()) {
+                modifier += this.getWeapon().getAttackValue();
+            }
+            if (this.getArmor()) {
+                modifier += this.getArmor().getAttackValue();
+            }
+        }
+        return this._attackValue + modifier;
     },
     attack: function(target) {
         // If the target is destructible, calculate the damage
@@ -184,6 +206,10 @@ Game.EntityMixins.InventoryHolder = {
         return false;
     },
     removeItem: function(i) {
+        // If we can equip items, then make sure we unequip the item we are removing.
+        if (this._items[i] && this.hasMixin(Game.EntityMixins.Equipper)) {
+            this.unequip(this._items[i]);
+        }
         // Simply clear the inventory slot.
         this._items[i] = null;
     },
@@ -287,6 +313,41 @@ Game.EntityMixins.CorpseDropper = {
                     name: this._name + ' corpse',
                     foreground: this._foreground
                 }));
+        }
+    }
+};
+
+Game.EntityMixins.Equipper = {
+    name: 'Equipper',
+    init: function(template) {
+        this._weapon = null;
+        this._armor = null;
+    },
+    wield: function(item) {
+        this._weapon = item;
+    },
+    unwield: function() {
+        this._weapon = null;
+    },
+    wear: function(item) {
+        this._armor = item;
+    },
+    takeOff: function() {
+        this._armor = null;
+    },
+    getWeapon: function() {
+        return this._weapon;
+    },
+    getArmor: function() {
+        return this._armor;
+    },
+    unequip: function(item) {
+        // Helper function to be called before getting rid of an item.
+        if (this._weapon === item) {
+            this.unwield();
+        }
+        if (this._armor === item) {
+            this.takeOff();
         }
     }
 };
