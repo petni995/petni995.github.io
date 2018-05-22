24f4e92c88cbf282e2fa7bdb6a870cf75a26fcec
Peter Nilsson <petni995@gmail.com>
2018-05-22 14:52:52 +0200

    moved player actor to top

--- Rouge/assets/entitymixins.js
+++ Rouge/assets/entitymixins.js
@@ -3,6 +3,33 @@
 Game.EntityMixins = {};
 
 
+// Main player's actor mixin
+Game.EntityMixins.PlayerActor = {
+    name: 'PlayerActor',
+    groupName: 'Actor',
+    act: function() {
+      if (this._acting) {
+          return;
+      }
+      this._acting = true;
+      this.addTurnHunger();
+      // Detect if the game is over
+      if (!this.isAlive()) {
+          Game.Screen.playScreen.setGameEnded(true);
+          // Send a last message to the player
+          Game.sendMessage(this, 'Press [Enter] to continue!');
+      }        // Re-render the screen
+      Game.refresh();
+      // Lock the engine and wait asynchronously
+      // for the player to press a key.
+      this.getMap().getEngine().lock();
+      // Clear the message queue
+      this.clearMessages();
+      this._acting = false;
+    }
+}
+
+
 // This signifies our entity posseses a field of vision of a given radius.
 Game.EntityMixins.Sight = {
     name: 'Sight',
@@ -205,31 +232,6 @@ Game.EntityMixins.AutoExplore = {
     },
 };
 
-// Main player's actor mixin
-Game.EntityMixins.PlayerActor = {
-    name: 'PlayerActor',
-    groupName: 'Actor',
-    act: function() {
-      if (this._acting) {
-          return;
-      }
-      this._acting = true;
-      this.addTurnHunger();
-      // Detect if the game is over
-      if (!this.isAlive()) {
-          Game.Screen.playScreen.setGameEnded(true);
-          // Send a last message to the player
-          Game.sendMessage(this, 'Press [Enter] to continue!');
-      }        // Re-render the screen
-      Game.refresh();
-      // Lock the engine and wait asynchronously
-      // for the player to press a key.
-      this.getMap().getEngine().lock();
-      // Clear the message queue
-      this.clearMessages();
-      this._acting = false;
-    }
-}
 
 
 Game.EntityMixins.FungusActor = {
