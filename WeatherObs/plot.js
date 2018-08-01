36212d5729ed8fd7f5c24653fcab536e631c4fd4
Peter Nilsson <petni995@gmail.com>
2018-08-01 10:30:18 +0200

    weatherobs: new folder + mouseover + cleaned up

--- WeatherObs/plot.js
+++ WeatherObs/plot.js
@@ -1,326 +1,4 @@
 
-function plotTemp(dataset, title) {
-
-// Define the resolution
-var width = 800;
-var height = 400;
-
-// Create the SVG 'canvas'
-var svg = d3.select("#chart")
-    .append("svg")
-    .attr("width", width )
-    .attr("height", height)
-
-
-// Define the padding around the graph
-var padding = 60;
-
-// Set the scales
-var minDate = d3.min(dataset, function(d) { return d.x; });
-// minDate.setDate(minDate.getDate() - 1);
-
-var maxDate = d3.max(dataset, function(d) { return d.x; });
-
-var xScale = d3.time.scale()
-    .domain([minDate, maxDate])
-    .range([padding, width - padding]);
-
-var yScale = d3.scale.linear()
-    .domain([d3.min(dataset, function(d) { return d.y; }), d3.max(dataset, function(d) { return d.y; })])
-    .range([height - padding, padding]);
-
-// x-axis
-var format = d3.time.format("%d %b");
-var xAxis = d3.svg.axis()
-    .scale(xScale)
-    .orient("bottom")
-    .tickFormat(format)
-    .ticks(d3.time.days, 1);
-
-svg.append("g")
-    .attr("class", "axis x-axis")
-    .attr("transform", "translate(0," + (height - padding) + ")")
-    .call(xAxis)
-    .selectAll("text")
-    .attr("y", 0)
-    .attr("x", 9)
-    .attr("dy", ".35em")
-    .attr("transform", "rotate(90)")
-    .style("text-anchor", "start");
-
-// y-axis
-var yAxis = d3.svg.axis()
-    .scale(yScale)
-    .orient("left")
-    .tickFormat(function (d) { return d; })
-    .tickSize(5, 5, 0)
-    .ticks(5); // set rough # of ticks
-
-svg.append("g")
-    .attr("class", "axis y-axis")
-    .attr("transform", "translate(" + padding + ",0)")
-    .call(yAxis);
-
-// draw line graph
-var line = d3.svg.line()
-    .x(function(d) {
-        return xScale(d.x);
-    })
-    .y(function(d) {
-        return yScale(d.y);
-    });
-
-svg.append("svg:path").attr("d", line(dataset));
-
-// plot circles
-svg.selectAll("circle")
-    .data(dataset)
-    .enter()
-    .append("circle")
-    .attr("class", "data-point")
-    .attr("cx", function(d) {
-        return xScale(d.x);
-    })
-    .attr("cy", function(d) {
-        return yScale(d.y);
-    })
-    .attr("r", 3);
-
-svg.append("text")
-        .attr("x", (width / 2))
-        .attr("y", 50 )
-        .attr("text-anchor", "middle")
-        .style("font-size", "16px")
-        .style("text-decoration", "underline")
-        .text(title);
-
-}
-
-
-function plotDual(dataset, dataset2, title) {
-
-// Define the resolution
-var width = 800;
-var height = 400;
-
-// Create the SVG 'canvas'
-var svg = d3.select("#chart")
-    .append("svg")
-    .attr("width", width )
-    .attr("height", height)
-
-
-// Define the padding around the graph
-var padding = 60;
-
-// Set the scales
-var minDate = d3.min(dataset, function(d) { return d.x; });
-// minDate.setDate(minDate.getDate() - 1);
-
-var maxDate = d3.max(dataset, function(d) { return d.x; });
-
-var xScale = d3.time.scale()
-    .domain([minDate, maxDate])
-    .range([padding, width - padding]);
-
-var yScale = d3.scale.linear()
-    .domain([d3.min(dataset, function(d) { return d.y; }), d3.max(dataset, function(d) { return d.y; })])
-    .range([height - padding, padding]);
-
-// x-axis
-var format = d3.time.format("%d %b %H");
-var xAxis = d3.svg.axis()
-    .scale(xScale)
-    .orient("bottom")
-    .tickFormat(format)
-    .ticks(d3.time.hours, 1);
-
-svg.append("g")
-    .attr("class", "axis x-axis")
-    .attr("transform", "translate(0," + (height - padding) + ")")
-    .call(xAxis)
-    .selectAll("text")
-    .attr("y", 0)
-    .attr("x", 9)
-    .attr("dy", ".35em")
-    .attr("transform", "rotate(90)")
-    .style("text-anchor", "start");
-
-// y-axis
-var yAxis = d3.svg.axis()
-    .scale(yScale)
-    .orient("left")
-    .tickFormat(function (d) { return d; })
-    .tickSize(5, 5, 0)
-    .ticks(5); // set rough # of ticks
-
-svg.append("g")
-    .attr("class", "axis y-axis")
-    .attr("transform", "translate(" + padding + ",0)")
-    .call(yAxis);
-
-// draw line graph
-var line = d3.svg.line()
-    .x(function(d) {
-        return xScale(d.x);
-    })
-    .y(function(d) {
-        return yScale(d.y);
-    });
-
-svg.append("svg:path").attr("d", line(dataset));
-svg.append("svg:path").attr("d", line(dataset2));
-
-// plot circles
-svg.selectAll(".data-point")
-    .data(dataset)
-    .enter()
-    .append("circle")
-    .attr("class", "data-point")
-    .attr("cx", function(d) {
-        return xScale(d.x);
-    })
-    .attr("cy", function(d) {
-        return yScale(d.y);
-    })
-    .attr("r", 3);
-
-svg.selectAll(".data-point2")
-    .data(dataset2)
-    .enter()
-    .append("circle")
-    .attr("class", "data-point2")
-    .attr("cx", function(d) {
-        return xScale(d.x);
-    })
-    .attr("cy", function(d) {
-        return yScale(d.y);
-    })
-    .attr("r", 3);
-
-svg.append("text")
-        .attr("x", (width / 2))
-        .attr("y", 50 )
-        .attr("text-anchor", "middle")
-        .style("font-size", "16px")
-        .style("text-decoration", "underline")
-        .text(title);
-
-}
-
-function plotDual2(dataset, dataset2, title, plotcomb) {
-
-// Define the resolution
-var width = 800;
-var height = 400;
-
-// Create the SVG 'canvas'
-var svg = d3.select("#chart")
-    .append("svg")
-    .attr("width", width )
-    .attr("height", height)
-
-
-// Define the padding around the graph
-var padding = 60;
-
-// Set the scales
-var minDate = d3.min(dataset, function(d) { return d.x; });
-// minDate.setDate(minDate.getDate() - 1);
-
-var maxDate = d3.max(dataset, function(d) { return d.x; });
-
-var xScale = d3.time.scale()
-    .domain([minDate, maxDate])
-    .range([padding, width - padding]);
-
-var yScale = d3.scale.linear()
-    .domain([d3.min(plotcomb, function(d) { return d.y; }), d3.max(plotcomb, function(d) { return d.y; })])
-    .range([height - padding, padding]);
-
-// x-axis
-var format = d3.time.format("%d %b %H");
-var xAxis = d3.svg.axis()
-    .scale(xScale)
-    .orient("bottom")
-    .tickFormat(format)
-    .ticks(d3.time.hours, 1);
-
-svg.append("g")
-    .attr("class", "axis x-axis")
-    .attr("transform", "translate(0," + (height - padding) + ")")
-    .call(xAxis)
-    .selectAll("text")
-    .attr("y", 0)
-    .attr("x", 9)
-    .attr("dy", ".35em")
-    .attr("transform", "rotate(90)")
-    .style("text-anchor", "start");
-
-// y-axis
-var yAxis = d3.svg.axis()
-    .scale(yScale)
-    .orient("left")
-    .tickFormat(function (d) { return d; })
-    .tickSize(5, 5, 0)
-    .ticks(5); // set rough # of ticks
-
-svg.append("g")
-    .attr("class", "axis y-axis")
-    .attr("transform", "translate(" + padding + ",0)")
-    .call(yAxis);
-
-// draw line graph
-var line = d3.svg.line()
-    .x(function(d) {
-        return xScale(d.x);
-    })
-    .y(function(d) {
-        return yScale(d.y);
-    });
-
-svg.append("svg:path").attr("d", line(dataset));
-svg.append("svg:path").attr("d", line(dataset2));
-
-// plot circles
-svg.selectAll(".data-point")
-    .data(dataset)
-    .enter()
-    .append("circle")
-    .attr("class", "data-point")
-    .attr("cx", function(d) {
-        return xScale(d.x);
-    })
-    .attr("cy", function(d) {
-        return yScale(d.y);
-    })
-    .attr("r", 3);
-
-svg.selectAll(".data-point2")
-    .data(dataset2)
-    .enter()
-    .append("circle")
-    .attr("class", "data-point2")
-    .attr("cx", function(d) {
-        return xScale(d.x);
-    })
-    .attr("cy", function(d) {
-        return yScale(d.y);
-    })
-    .attr("r", 3);
-
-svg.append("text")
-        .attr("x", (width / 2))
-        .attr("y", 50 )
-        .attr("text-anchor", "middle")
-        .style("font-size", "16px")
-        .style("text-decoration", "underline")
-        .text(title);
-
-}
-
-
-
 function plotDualMonth(dataset, dataset2, title) {
 
 // Define the resolution
@@ -333,6 +11,7 @@ var svg = d3.select("#chart")
     .attr("width", width )
     .attr("height", height)
 
+var formatTime = d3.time.format("%e %B");
 
 // Define the padding around the graph
 var padding = 60;
@@ -343,12 +22,18 @@ var minDate = d3.min(dataset, function(d) { return d.x; });
 
 var maxDate = d3.max(dataset, function(d) { return d.x; });
 
+// Define the div for the tooltip
+var div = d3.select("#chart").append("div")
+    .attr("class", "tooltip")
+    .style("opacity", 0);
+
+
 var xScale = d3.time.scale()
     .domain([minDate, maxDate])
     .range([padding, width - padding]);
 
 var yScale = d3.scale.linear()
-    .domain([d3.min(dataset, function(d) { return d.y; }), d3.max(dataset, function(d) { return d.y; })])
+    .domain([d3.min(dataset, function(d) { return d.y; }), d3.max(dataset, function(d) { return d.y; }) + 5])
     .range([height - padding, padding]);
 
 // x-axis
@@ -407,7 +92,20 @@ svg.selectAll(".data-point")
     .attr("cy", function(d) {
         return yScale(d.y);
     })
-    .attr("r", 3);
+    .attr("r", 3)
+    .on("mouseover", function(d) {
+            div.transition()
+                .duration(200)
+                .style("opacity", .9);
+            div	.html(formatTime(d.x) + "<br/>"  + d.y)
+                .style("left", (d3.event.pageX) + "px")
+                .style("top", (d3.event.pageY - 28) + "px");
+            })
+    .on("mouseout", function(d) {
+        div.transition()
+            .duration(500)
+            .style("opacity", 0);
+    });
 
 svg.selectAll(".data-point2")
     .data(dataset2)
@@ -420,7 +118,20 @@ svg.selectAll(".data-point2")
     .attr("cy", function(d) {
         return yScale(d.y);
     })
-    .attr("r", 3);
+    .attr("r", 3)
+    .on("mouseover", function(d) {
+            div.transition()
+                .duration(200)
+                .style("opacity", .9);
+            div	.html(formatTime(d.x) + "<br/>"  + d.y)
+                .style("left", (d3.event.pageX) + "px")
+                .style("top", (d3.event.pageY - 28) + "px");
+            })
+    .on("mouseout", function(d) {
+        div.transition()
+            .duration(500)
+            .style("opacity", 0);
+    });
 
 svg.append("text")
         .attr("x", (width / 2))
@@ -459,7 +170,7 @@ var xScale = d3.time.scale()
     .range([padding, width - padding]);
 
 var yScale = d3.scale.linear()
-    .domain([d3.min(dataset, function(d) { return d.y; }), d3.max(dataset, function(d) { return d.y; })])
+    .domain([d3.min(dataset, function(d) { return d.y; }), d3.max(dataset, function(d) { return d.y; }) + 5])
     .range([height - padding, padding]);
 
 // x-axis
@@ -545,7 +256,7 @@ var xScale = d3.time.scale()
     .range([padding, width - padding]);
 
 var yScale = d3.scale.linear()
-    .domain([d3.min(dataset3, function(d) { return d.y; }), d3.max(dataset3, function(d) { return d.y; })])
+    .domain([d3.min(dataset3, function(d) { return d.y; }), d3.max(dataset3, function(d) { return d.y; }) + 5])
     .range([height - padding, padding]);
 
 // x-axis
@@ -658,7 +369,7 @@ var xScale = d3.time.scale()
     .range([padding, width - padding]);
 
 var yScale = d3.scale.linear()
-    .domain([d3.min(dataset, function(d) { return d.y; }), d3.max(dataset, function(d) { return d.y; })])
+    .domain([d3.min(dataset, function(d) { return d.y; }), d3.max(dataset, function(d) { return d.y; }) + 5])
     .range([height - padding, padding]);
 
 // x-axis
--- WeatherObs/readData.js
+++ WeatherObs/readData.js
@@ -82,25 +82,6 @@ $.getJSON( "https://opendata-download-metobs.smhi.se/api/version/latest/paramete
 });
 
 
-
-$.getJSON( "https://opendata-download-metobs.smhi.se/api/version/latest/parameter/21/station/97280/period/latest-months/data.json", function( data ) {
-  var plot1 = []
-  var plot2 = []
-  var observTemp = data
-  for (var i = 0; i < observTemp.value.length; i++) {
-    var d = new Date(observTemp.value[i].date)
-    plot1.push({'x': d,'y':Number(observTemp.value[i].value)})
-  }
-  $.getJSON( "https://opendata-download-metobs.smhi.se/api/version/latest/parameter/4/station/97280/period/latest-day/data.json", function( data ) {
-    var observTemp2 = data
-    for (var i = 0; i < observTemp2.value.length; i++) {
-      var d = new Date(observTemp2.value[i].date)
-      plot2.push({'x': d,'y':Number(observTemp2.value[i].value)})
-    }
-    plotDualMonth2(plot1, plot2, "ByVind latest dygn - Adelsö");
-  });
-});
-
 // Nederbörd
 
 var nedbPlotObsDay = [];
@@ -124,38 +105,6 @@ $.getJSON( "https://opendata-download-metobs.smhi.se/api/version/latest/paramete
 
 });
 
-
-
-// Molnmängd Total
-
-
-$.getJSON( "https://opendata-download-metobs.smhi.se/api/version/latest/parameter/16/station/98210/period/latest-day/data.json", function( data ) {
-  var plot1 = []
-  var plot2 = []
-  var plot3 = []
-
-  var observTemp = data
-  for (var i = 0; i < observTemp.value.length; i++) {
-    var d = new Date(observTemp.value[i].date)
-    plot1.push({'x': d,'y':((Number(observTemp.value[i].value)/100) * 8)})
-  }
-  $.getJSON( "https://opendata-download-metobs.smhi.se/api/version/latest/parameter/16/station/98210/period/latest-day/data.json", function( data ) {
-    var observTemp2 = data
-    for (var i = 0; i < observTemp2.value.length; i++) {
-      var d = new Date(observTemp2.value[i].date)
-      plot2.push({'x': d,'y':Number(observTemp2.value[i].value)})
-    }
-    $.getJSON( "https://opendata-download-metfcst.smhi.se/api/category/pmp2g/version/2/geotype/point/lon/18.054399/lat/59.342007/data.json", function( data ) {
-      var plot3 = []
-      var temp = data
-      for (var i = 0; i < temp.timeSeries.length; i++) {
-        var d = new Date(temp.timeSeries[i].validTime)
-        plot3.push({'x': d,'y':Number(temp.timeSeries[i].parameters[7].values[0])})
-      }
-      plotTriple(plot1, plot2, plot3, "Moln total % obs and prognos");
-      });
-  });
-});
 
 // Snödjup
 
